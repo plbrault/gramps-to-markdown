86b1d2313a0f37cdaf22d1b002fc3f231521bdaf
Pier-Luc Brault <pier-luc@brault.me>
2022-06-22 21:45:34 -0400

    Create objects dictionary

--- src/classes/Database.js
+++ src/classes/Database.js
@@ -1,18 +1,37 @@
 import { XMLParser } from 'fast-xml-parser';
 
+function createObjects(rawData) {
+  const objects = {};
+
+  Object.keys(rawData.database)
+    .filter(key => ['events', 'people', 'families', 'citations', 'sources', 'places', 'notes'].includes(key))
+    .forEach(key => {
+      const objType = Object.keys(rawData.database[key])[0];
+      rawData.database[key][objType].forEach(obj => {
+        objects[obj.handle] = { handle: obj.handle, type: objType, raw: obj, data: {} };
+      });
+    });
+
+  return objects;
+}
+
 class Database {
   #data;
 
   constructor(xmlData) {
     this.#prepareDatabase(xmlData);
 
-    console.log(this.#data);
+    //console.log(this.#data.database.people);
   }
-  
+
   #prepareDatabase(xmlData) {
-    const xmlParser = new XMLParser();
+    const xmlParser = new XMLParser({ ignoreAttributes: false, attributeNamePrefix: '' });
     const rawData = xmlParser.parse(xmlData);
 
+    const objects = createObjects(rawData);
+
+    console.log(objects);
+  
     this.#data = rawData;
   }
 }
