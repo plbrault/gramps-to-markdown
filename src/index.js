68cc75b7bddd07da2deafe3a9e57799f84f38a50
Pier-Luc Brault <pier-luc@brault.me>
2022-06-24 14:52:32 -0400

    Output to files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import fs from 'fs';
 import readXmlFromFile from './data/readXmlFromFile.js';
 import Database from './data/Database.js';
+
 import personTpl from './templates/personTpl.js';
 
-const [, , inputFile] = process.argv;
+const [, , inputFile, outputDir = './output'] = process.argv;
 
 if (inputFile === undefined) {
   console.error('Input argument missing.');
@@ -12,8 +13,13 @@ if (inputFile === undefined) {
 const xmlData = await readXmlFromFile(inputFile);
 const database = new Database(xmlData);
 
+if (!fs.existsSync(outputDir)) {
+  fs.mkdirSync(outputDir);
+}
+
 database.getPeople().forEach((person) => {
-  console.log(personTpl(person));
+  const markdown = personTpl(person);
+  fs.writeFileSync(`${outputDir}/${person.id}.md`, markdown, 'utf8');
 });
 
 //fs.writeFile('testData/test.md', test, 'utf8', () => {});
