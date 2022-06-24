e7db7e81168be9fd01d7b9324ff13811b5ef9e6c
Pier-Luc Brault <pier-luc@brault.me>
2022-06-24 12:47:18 -0400

    Output family complete

--- src/templates/familyTpl.js
+++ src/templates/familyTpl.js
@@ -24,11 +24,19 @@ export default (family, mainPerson) => {
     formattedEvents += '\n';
   }
 
+  let formattedChildren = '';
+  if (family.children.length > 0) {
+    formattedChildren += '#### Children\n';
+    family.children.forEach((child) => {
+      formattedChildren += `\n  * ${nameTpl(findPreferredName(child))}`;
+    });
+  }
+
   return (
     /* eslint-disable indent */
 `### With ${otherPersonName}
 
-${formattedEvents}${notes}
+${formattedEvents}${formattedChildren}${notes}
 `
     /* eslint-enable indent */
   );
