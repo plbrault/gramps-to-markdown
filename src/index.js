31d8ccd456d05256c5dc09ed6a51f70137025358
Pier-Luc Brault <pier-luc@brault.me>
2022-06-25 01:25:54 -0400

    Add CLI command and improve feedback (#6)

--- src/index.js
+++ src/index.js
@@ -9,8 +9,21 @@ import peopleTpl from './templates/peopleTpl.js';
 
 const [, , inputFile, outputDir = './output', optionsJSON = '{}'] = process.argv;
 
+function printUsage() {
+  console.log('');
+  console.log('Usage:');
+  console.log('    gramps2md inputFilePath [outputDirPath] [options]');
+  console.log('');
+  console.log('Examples:');
+  console.log('    gramps2md genealogy.gramps');
+  console.log('    gramps2md genealogy.gramps output');
+  console.log('    gramps2md genealogy.gramps output \'{ "urlPrefix": "/genealogy", "urlExt": ".html", "languages": ["en", "fr"] }\'');
+}
+
 if (inputFile === undefined) {
-  console.error('Input argument missing.');
+  console.error('You must provide an input file.');
+  printUsage();
+  process.exit(1);
 }
 
 const options = {
@@ -19,6 +32,8 @@ const options = {
   urlExt: '.md',
   ...JSON.parse(optionsJSON),
 };
+
+console.log('Options used: ', options);
 
 const xmlData = await readXmlFromFile(inputFile);
 const database = new Database(xmlData);
@@ -33,13 +48,17 @@ if (!fs.existsSync(outputDir)) {
 database.getPeople().forEach((person) => {
   if (options.languages.length === 1) {
     const markdown = personTpl(person, {
-      createLink: createLinkFunctions[0], t: translateFunctions[0]
+      createLink: createLinkFunctions[0], t: translateFunctions[0],
     });
-    fs.writeFileSync(`${outputDir}/${person.id}.md`, markdown, 'utf8');
+    const filename = `${outputDir}/${person.id}.md`;
+    fs.writeFileSync(filename, markdown, 'utf8');
+    console.log(`Created file ${filename}`);
   } else {
     translateFunctions.forEach((t, id) => {
       const markdown = personTpl(person, { createLink: createLinkFunctions[id], t });
-      fs.writeFileSync(`${outputDir}/${person.id}-${options.languages[id]}.md`, markdown, 'utf8');
+      const filename = `${outputDir}/${person.id}-${options.languages[id]}.md`;
+      fs.writeFileSync(filename, markdown, 'utf8');
+      console.log(`Created file ${filename}`);
     });
   }
 });
@@ -48,10 +67,14 @@ if (options.languages.length === 1) {
   const markdown = peopleTpl(database.getPeople(), {
     createLink: createLinkFunctions[0], t: translateFunctions[0],
   });
-  fs.writeFileSync(`${outputDir}/individuals.md`, markdown, 'utf8');
+  const filename = `${outputDir}/individuals.md`;
+  fs.writeFileSync(filename, markdown, 'utf8');
+  console.log(`Created file ${filename}`);
 } else {
   translateFunctions.forEach((t, id) => {
     const markdown = peopleTpl(database.getPeople(), { createLink: createLinkFunctions[id], t });
-    fs.writeFileSync(`${outputDir}/individuals-${options.languages[id]}.md`, markdown, 'utf8');
+    const filename = `${outputDir}/individuals-${options.languages[id]}.md`;
+    fs.writeFileSync(filename, markdown, 'utf8');
+    console.log(`Created file ${filename}`);
   });
 }
