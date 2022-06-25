f24932ca80cb5678e22008c56a54e6177f95f568
Pier-Luc Brault <pier-luc@brault.me>
2022-06-25 15:01:23 -0400

    Add frontmatter to people file

--- src/templates/peopleTpl.js
+++ src/templates/peopleTpl.js
@@ -1,9 +1,21 @@
 import findPreferredName from './utilities/findPreferredName.js';
 import nameTpl from './nameTpl.js';
 
-export default (people, { createLink, t }) => (
+export default (people, { createLink, t, language, addFrontmatter, extraFrontmatterFields }) => {
+  let frontmatter = '';
+  if (addFrontmatter) {
+    frontmatter += '---\n';
+    frontmatter += 'type: people\n';
+    frontmatter += `language: ${language}\n`;
+    Object.keys(extraFrontmatterFields).forEach((field) => {
+      frontmatter += `${field}: ${extraFrontmatterFields[field]}\n`;
+    });
+    frontmatter += '---\n\n';
+  }
+
+  return (
   /* eslint-disable indent */
-`# ${t('All Individuals')}
+`${frontmatter}# ${t('All Individuals')}
 ${
   people
     .map((person) => `${createLink(person, nameTpl(findPreferredName(person)))}`)
@@ -13,4 +25,5 @@ ${
 
 `
   /* eslint-enable indent */
-);
+  );
+};
